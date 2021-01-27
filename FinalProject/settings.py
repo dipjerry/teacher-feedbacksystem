2e3c2b0c2c256cc5cab5d527a03614f0c200f2b2
dipjerry <49409502+dipjerry@users.noreply.github.com>
2021-01-27 17:41:50 +0530

    Update settings.py

--- FinalProject/settings.py
+++ FinalProject/settings.py
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-django_heroku.settings(locals())
+# django_heroku.settings(locals())
 
 # for email
 
